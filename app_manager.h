6750dfa7c2ad221ab48010ba24dd2f5c625e116b
A_lonely_tree <1285168990@qq.com>
2024-10-29 10:14:30 +0800

    丰富完善了注释

--- app_manager.h
+++ app_manager.h
@@ -19,23 +19,55 @@
 //函数指针
 typedef void (*App_Body_Point)(void);
 
-//任务延时计数器
+/**
+ * @brief 任务延时计数器
+ * @note 需要放在定时器中断中调用
+ * @note 每次调用所有挂起的任务的延时计数减1
+ * */
 void Task_Delay_Update(void);
-//根据任务名称获取任务索引
+
+/**
+ * @brief 根据任务名称获取任务索引
+ * @param name 任务名称
+ * @return 任务索引
+ * */
 uint8_t Get_AppIndex_ByName(App_Body_Point name);
-// 获取当前任务列表的长度
-uint8_t Get_AppList_Length(void);
-// 根据任务索引获取任务名称
+
+/**
+ * @brief 根据任务索引获取任务名称
+ * @param index 任务索引
+ * @return 任务函数指针
+ * */
 App_Body_Point Get_AppName_ByIndex(uint8_t index);
-//根据任务名称获取任务索引
-uint8_t Get_AppIndex_ByName(App_Body_Point name);
-// 函数状态更新
+
+/**
+ * @brief 获取任务列表长度
+ * @return 任务列表长度
+ */
+uint8_t Get_AppList_Length(void);
+
+/**
+ * @brief 函数状态更新
+ * @note 该函数再主循环调用，当延时计数为零时，自动更改任务状态为运行
+ * @note 但是该函数并不能运行任务，需要调用(App_Manager_Control)函数
+ */
 void App_Change_To_Running(void);
-// 任务管理器
+
+/**
+ * @brief 任务管理器控制函数
+ * @note 该函数在主循环中调用，运行所有运行状态的函数
+ * @note 运行完任务后，会自动重载延时计数器，同时将状态更改为挂起
+ */
 void App_Manager_Control(void);
-// 任务初始化
+
+/**
+ * @brief 任务初始化
+ * @brief 初始化所有需要初始化的任务
+ */
 void App_Manager_Init(void);
+
 /*----------------------- task States -----------------------*/
+//任务状态枚举
 typedef enum{
     /*任务就绪*/
     TASK_READY = 0,
@@ -49,6 +81,7 @@ typedef enum{
     TASK_ERROR
 }Task_States;
 /*---------------------- task struct ------------------------*/
+//任务结构体
 typedef struct{
     //初始化函数
     App_Body_Point task_init;
@@ -61,6 +94,7 @@ typedef struct{
     //任务需要的函数体
     App_Body_Point app_task;
 }App_Task;
+//任务列表
 extern App_Task task_list[];
 
 #endif
